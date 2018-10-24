9827c5916c27fa275c1bc3927843701a1932c998
Artem Borisov <dedsa2002@gmail.com>
2018-10-24 22:06:38 +0200

    hi6250: import berkeley-dev features

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -23,10 +23,20 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-# inherit from common kirin970-common
--include device/huawei/kirin970-common/BoardConfigCommon.mk
-
 DEVICE_PATH := device/huawei/hi6250
+
+# Arch
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a15
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := berkeley,kirin970,hi6250
@@ -34,3 +44,37 @@ TARGET_OTA_ASSERT_DEVICE := berkeley,kirin970,hi6250
 # Camera
 TARGET_CAMERA_NEEDS_ADD_STATES_IN_ENUMERATE := true
 
+# Display
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x2080000U
+
+# File System
+TARGET_EXFAT_DRIVER := exfat
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_NO_KERNEL := false
+TARGET_PREBUILT_KERNEL := /dev/null
+
+# Properties
+TARGET_SYSTEM_PROP := $(VENDOR_PATH)/system.prop
+
+# Recovery
+BOARD_PROVIDES_BOOTLOADER_MESSAGE := true
+TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/etc/fstab.kirin970
+
+# Release Tools
+TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)/releasetools
+
+# SELinux
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
+
+# Shims
+TARGET_LD_SHIM_LIBS := \
+    /system/lib64/libdisplayenginesvc_1_0.so|libshims_hwsmartdisplay_jni.so \
+    /system/lib64/libdisplayenginesvc_1_1.so|libshims_hwsmartdisplay_jni.so \
+    /system/lib64/libhwsmartdisplay_jni.so|libshims_hwsmartdisplay_jni.so \
+    /vendor/bin/hw/vendor.huawei.hardware.hisupl@1.0-service|libshims_hisupl.so
+
+# Vintf
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(VENDOR_PATH)/framework_manifest.xml
--- device.mk
+++ device.mk
@@ -28,6 +28,14 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 
+# APN
+PRODUCT_COPY_FILES += \
+        device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Audio
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
+
 # Blobs
 $(call inherit-product-if-exists, vendor/huawei/hi6250/hi6250-vendor.mk)
 
@@ -35,12 +43,21 @@ $(call inherit-product-if-exists, vendor/huawei/hi6250/hi6250-vendor.mk)
 PRODUCT_PACKAGES += \
     Snap
 
-# Device
-$(call inherit-product, device/huawei/kirin970-common/kirin970.mk)
-
 # Display
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/compatibility_matrix.xml:system/compatibility_matrix.xml
+
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/fingerprint.kl:system/usr/keylayout/fingerprint.kl
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -57,9 +74,79 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
+# Audio
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
+
 
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# Properties
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.version.all_codenames=$(PLATFORM_VERSION_ALL_CODENAMES) \
+    ro.build.version.codename=$(PLATFORM_VERSION_CODENAME) \
+    ro.build.version.huawei=8.0.0 \
+    ro.build.version.huawei1=8.1.0 \
+    ro.build.version.release=$(PLATFORM_VERSION) \
+    ro.build.version.sdk=$(PLATFORM_SDK_VERSION) \
+    ro.cust.cdrom=/dev/null
+
+# Radio
+PRODUCT_PACKAGES += \
+    qti-telephony-common
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.bcm43xx.rc \
+    init.hisi.rc \
+    init.kirin970.rc \
+    init.kirin970.ab.rc \
+    init.kirin970.environ.rc
+
+# Recovery
+PRODUCT_PACKAGES += \
+    resize2fs_static
+
+# Release Tools
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/releasetools/releasetools.kirin970.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/releasetools.kirin970.sh
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
+# Selinux
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sepolicy/27.0.cil:$(TARGET_COPY_OUT_SYSTEM)/etc/selinux/mapping/27.0.cil
+
+# Shims
+PRODUCT_PACKAGES += \
+    libshims_hisupl \
+    libshims_hwsmartdisplay_jni
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.kirin970
+
+# VNDK
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vndk-compat/ld.config.26.txt:system/etc/ld.config.26.txt \
+    $(LOCAL_PATH)/vndk-compat/llndk.libraries.26.txt:system/etc/llndk.libraries.26.txt \
+    $(LOCAL_PATH)/vndk-compat/vndksp.libraries.26.txt:system/etc/vndksp.libraries.26.txt \
+    $(LOCAL_PATH)/vndk-compat/ld.config.27.txt:system/etc/ld.config.27.txt
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi.hostapd@1.0 \
+    android.hardware.wifi@1.2
